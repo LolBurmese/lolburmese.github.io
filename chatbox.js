d654db740c81e16e299c2ded3fc8062707ddc7db
MyintKoKoOo <MyintKoKoOo@users.noreply.github.com>
2020-08-22 14:16:05 +0630

    testing

--- chatbox.js
+++ chatbox.js
@@ -25,3 +25,31 @@ var user_bot = [
 
 var chat_text = document.getElementsByClassName("chat_text")[0];
 var textbox = document.getElementsByClassName("textbox")[0];
+
+const send_click = () =>{
+	var usertextdiv = document.createElement("div");
+	usertextdiv.setAttribute("class","usertextdiv");
+	var usertext = document.createElement("div");
+	usertext.setAttribute("class","usertext");
+	usertextdiv.appendChild(usertext);
+	usertext.textContent = textbox.value;
+	chat_text.appendChild(usertextdiv);
+	textbox.value= "";
+
+	var bottextdiv = document.createElement("div");
+	bottextdiv.setAttribute("class","bottextdiv");
+	var bottext = document.createElement("div");
+	bottext.setAttribute("class","bottext");
+	bottextdiv.appendChild(bottext);
+	bottext.innerHTML = "<div class='dot3'><div class='fdot'>.</div><div class='sdot'>.</div><div class='tdot'>.</div></div>";
+	chat_text.appendChild(bottextdiv);
+}
+
+
+
+
+
+
+
+
+
