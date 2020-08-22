e142a6f2cb919b0454a56b2d6d1381f084a0e574
LolBurmese <64733989+LolBurmese@users.noreply.github.com>
2020-08-22 13:34:13 +0630

    testing

---
+++ chatbox.js
@@ -0,0 +1,27 @@
+var user_bot = [
+
+{
+	user : "hello",
+	bot : "hey"
+},
+
+{
+	user : "how are you",
+	bot : "I'm fine, and you?"
+},
+
+{
+	user : "fuck you",
+	bot : "Oh! you're rude, FUCK YOU!"
+},
+
+{
+	user : "lee",
+	bot : "South Foke!"
+}
+
+
+];
+
+var chat_text = document.getElementsByClassName("chat_text")[0];
+var textbox = document.getElementsByClassName("textbox")[0];
