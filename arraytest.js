f372490d4297d8ddabd23230624019a2a4173f9a
LolBurmese <64733989+LolBurmese@users.noreply.github.com>
2020-08-26 11:05:42 +0630

    Update arraytest.js

--- arraytest.js
+++ arraytest.js
@@ -1,35 +1,35 @@
 [
 {
-	name : "Kaung Si Thu",
-	phone : "09693234189"
+	"name" : "Kaung Si Thu",
+	"phone" : "09693234189"
 },
 {
-	name : "Aye Myat Thu",
-	phone : "09693234189"
+	"name" : "Aye Myat Thu",
+	"phone" : "09693234189"
 },
 {
-	name : "Aung Kyaw Phyo",
-	phone : "09693234189"
+	"name" : "Aung Kyaw Phyo",
+	"phone" : "09693234189"
 },
 {
-	name : "Su Myat Loe Oo",
-	phone : "09693234189"
+	"name" : "Su Myat Loe Oo",
+	"phone" : "09693234189"
 },
 {
-	name : "Myint Ko Ko Oo",
-	phone : "09693234189"
+	"name" : "Myint Ko Ko Oo",
+	"phone" : "09693234189"
 },
 {
-	name : "Min Thwin Khant",
-	phone : "09693234189"
+	"name" : "Min Thwin Khant",
+	"phone" : "09693234189"
 },
 {
-	name : "Kyaw Htake Tin Maung",
-	phone : "09693234189"
+	"name" : "Kyaw Htake Tin Maung",
+	"phone" : "09693234189"
 },
 {
-	name : "Kyaw Swar Win",
-	phone : "09693234189"
+	"name" : "Kyaw Swar Win",
+	"phone" : "09693234189"
 }
 
 
